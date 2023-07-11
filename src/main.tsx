b5a62bd32f0645da7bff7ef041509928bf014e61
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-07-11 14:50:39 +0400

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Story from "./Pages/Story";
+import Team from "./Pages/Team";
+import Retail from "./Pages/Retail";
+import Capital from "./Pages/Capital";
+import NotFoundPage from "./Pages/Error-page";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Story" element={<Story />} />
+      <Route path="/Team" element={<Team />} />
+      <Route path="/Retail" element={<Retail />} />
+      <Route path="/Capital" element={<Capital />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
 );
