4265d6bb8b67185d0a63a459c1041c77f21b50fe
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-11 12:47:55 +0400

    fixing bugs and improvement

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ import Capital from "./Pages/Capital";
 import NotFoundPage from "./Pages/Error-page";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/story" element={<Story />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/retail" element={<Retail />} />
-        <Route path="/capital" element={<Capital />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="story" element={<Story />} />
+          <Route path="team" element={<Team />} />
+          <Route path="retail" element={<Retail />} />
+          <Route path="capital" element={<Capital />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
