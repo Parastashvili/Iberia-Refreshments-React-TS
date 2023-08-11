ce1868a553999da7f8753f511d633d114cb3c625
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-11 14:51:32 +0400

    fixing bugs and improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Story from "./Pages/Story";
-import Team from "./Pages/Team";
-import Retail from "./Pages/Retail";
-import About from "./Pages/About";
+// import Home from "./Pages/Home";
+// import Story from "./Pages/Story";
+// import Team from "./Pages/Team";
+// import Retail from "./Pages/Retail";
+// import About from "./Pages/About";
 // import NotFoundPage from "./Pages/Error-page";
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="Story" element={<Story />} />
-            <Route path="Team" element={<Team />} />
-            <Route path="Retail" element={<Retail />} />
-            <Route path="About" element={<About />} />
-            {/* <Route path="*" element={<NotFoundPage />} /> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/">
+    //       <Route index element={<Home />} />
+    //       <Route path="Story" element={<Story />} />
+    //       <Route path="Team" element={<Team />} />
+    //       <Route path="Retail" element={<Retail />} />
+    //       <Route path="About" element={<About />} />
+    //       {/* <Route path="*" element={<NotFoundPage />} /> */}
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <BrowserRouter basename="/app">
+      <Routes>
+        <Route path="/" /> {<div>hello</div>}
+        <Route path="2" /> {<div>buy</div>}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
